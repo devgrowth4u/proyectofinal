5c8f01ba457bb955670829004d21f729671788cb
Cristopher Ponce <46466934+devgrowth4u@users.noreply.github.com>
2022-12-13 01:16:10 -0300

    Actualizacion 2

--- src/components/Content/content.jsx
+++ src/components/Content/content.jsx
@@ -1,17 +1,43 @@
+//import axios from "axios";
 import React from "react";
+//import { useState, useEffect } from 'react';
+import data from "../../api/data.json"
 
-
-
-const content = () => {
+function content() {
+    const posts=data.posts;
     return (
-        <div class="row content">
-        <div class="col-md-12">
-          <center>
-            <h1 class="bienvenida"> Bienvenidos </h1>
-            <h2 class="segundo-texto">Sistema de Ticket</h2>
-        </center>
+    <div className="row content">
+        <div className="col-md-12">
+        
+            <center>
+                <h1 class="bienvenida"> Bienvenidos </h1>
+            
+            </center>
+            <div id="carouselExampleIndicators" ClassName="carousel slide" data-bs-ride="carousel">
+                <div ClassName="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" ClassName="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div ClassName="carousel-inner">
+                    {posts.map((post) => (
+                            <div ClassName="carousel-item">
+                                <img src={post.img} ClassName="d-block w-100" alt="..."/>
+                           </div>  
+                    ))}
+                </div>
+                <button ClassName="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                    <span ClassName="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span ClassName="visually-hidden">Siguiente</span>
+                </button>
+                <button ClassName="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                    <span ClassName="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span ClassName="visually-hidden">Retocede</span>
+                </button>
+                </div>
+               
         </div>
-        </div>
+     </div>
     );
 }
 export default content;
--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,16 +1,14 @@
 import React from "react";
-import logo_fcdn from '../../components/images/logo.png';
 
 
 const footer = () => {
     return (
         <div className="Footer">
-             <div class="container">
-             <div class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom"> 
-                <a href="https://www.ciudaddelnino.cl/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-decoration-none">
-                <img src={logo_fcdn} alt="logo" />  </a>  
-                    <ul class="nav nav-pills">
-                        <div class="direccion">     
+             <div className="container">
+             <div className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom"> 
+                <a href="#home" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-decoration-none"> </a>  
+                    <ul className="nav nav-pills">
+                        <div className  ="direccion">     
                             <p> Dirección de Tecnología de la Información</p>
                         </div> 
                     </ul>
