619053a4a96942d74865b0f4f461688ba6dee91a
shascon <pherrera@ciudaddelnino.cl>
2022-12-16 17:39:38 -0300

    Home
    se quito card de ejemplo

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import Header from "../../components/Header/header";
 import Carousell from "../../components/Carousel/carousell";
 import Content from "../../components/Content/content";
 import Video from "../../components/Content/video";
-import Cardss from "../../components/Cards/card";
+
 import Footer from "../../components/Footer/footer";
 
 function Home() {
@@ -13,7 +13,7 @@ function Home() {
       <Carousell />
       <Content/>
       <Video />
-      <Cardss/>
+    
         <h3 className="titulo 2 col-md-12">Estamos en diez regiones, ejecutando más de un centenar de programas en nuestras tres áreas de trabajo.</h3>
       
       <Footer/>
